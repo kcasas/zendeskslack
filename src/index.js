4f31870f0e8b458bf8b51f509f618cfb9b8516fa
kcasas <karl.casas777@gmail.com>
2017-11-20 20:31:55 +0800

    working

--- src/index.js
+++ src/index.js
@@ -1,13 +1,61 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 4444;
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get('/ping', (req, res) => {
-  res.send('pong');
+  res.send(`pong \n token=${process.env.token}`);
 });
 
-app.get('/zendesk', (req, res) => {
-  console.log(req);
+app.post('/zendesk', (req, res) => {
+  const Botkit = require('botkit');
+
+  const slackController = Botkit.slackbot({
+    require_delivery: true
+  });
+
+  const bot = slackController.spawn({
+    token: process.env.token
+  });
+
+  bot.say({
+    username: 'zenbot',
+    icon_url: 'http://garden.zendesk.com/favicons/zendesk/apple-touch-icon.png',
+    channel: req.body.slack_channel,
+    text: `*[${req.body.status}]* <${req.body.url}|Ticket #${req.body.id}>`,
+    attachments: [
+      {
+        color: '#78a300',
+        title: req.body.title,
+        text: `${req.body.latest_comment} \n`,
+        author_name: `Requested by ${req.body.requester}`,
+        fields: [
+          {
+            title: "Priority",
+            value: req.body.priority
+          },
+          {
+            title: "Assignee",
+            value: req.body.assignee
+          }
+        ],
+        actions: [
+          {
+            name: 'edit_ticket',
+            text: 'Edit Ticket',
+            type: 'button',
+            value: 'edit_ticket'
+          }
+        ]
+      }
+    ]
+  });
+
+
+  console.log(req.body);
   res.send('pong');
 });
 
--- src/zenbot.js
+++ src/zenbot.js
@@ -1,17 +1,14 @@
 const Botkit = require('botkit');
 
-const slack_token = 'xoxb-274386247377-aVWXvKdeW2GBjDgPYgqWEF2Z';
-const slack_oauth = '';
-
 const slackController = Botkit.slackbot({
   require_delivery: true
 });
 
 const bot = slackController.spawn({
-  token: slack_token
+  token: process.env.token
 });
 
 bot.say({
-  text: 'fuck you',
+  text: 'hello',
   channel: 'zendesk'
 });
